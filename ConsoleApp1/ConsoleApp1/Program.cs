71a1b62179753a1dc03802739c9d94a98af79280
belal <belal.a75@gmail.com>
2018-11-06 10:39:52 +0200

    Q1+Q2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            AllPrimes(num);
+        }
+
+        static void AllPrimes(int num)
+        {
+            for (int i = 2; i <= num; i++)
+            {
+                
+                if (IsPrime(i))
+                {
+                    Console.Write(i + ",");
+                }
+            }
+            Console.WriteLine();
+        }
+        static bool IsPrime(int num)
+        {
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
